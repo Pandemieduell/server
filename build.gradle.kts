413b8862c603a5add19ddea37dd78053e816bf6f
Joshua Gleitze <dev@joshuagleitze.de>
2020-03-21 16:57:46 +0200

    Initial Deployment

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,12 +6,14 @@ plugins {
     id("com.google.cloud.tools.jib") version "2.1.0"
     id("com.palantir.docker-run") version "0.25.0"
     id("com.palantir.git-version") version "0.12.2"
+    id("com.cherryperry.gradle-file-encrypt") version "1.4.0"
 }
 
 group = "de.pandemieduell"
 version = versionDetails.gitHash
-val dockerImage = "docker.pkg.github.com/pandemieduell/server/${project.name}:${project.version}"
-val githubToken: String? by project
+val dockerImage = "pandemieduell/server:${project.version}"
+val dockerUser: String? by project
+val dockerPassword: String? by project
 
 java {
     sourceCompatibility = JavaVersion.VERSION_11
@@ -22,7 +24,9 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
@@ -32,14 +36,13 @@ tasks.withType<Test> {
     useJUnitPlatform()
 }
 
-
 jib {
     to {
         image = dockerImage
-        if (githubToken != null) {
+        if (dockerUser != null) {
             auth {
-                username = "ci"
-                password = githubToken
+                username = dockerUser
+                password = dockerPassword
             }
         }
     }
@@ -48,6 +51,12 @@ jib {
 dockerRun {
     name = project.name
     image = dockerImage
+}
+
+gradleFileEncrypt {
+    files = fileTree("src/deployment/kubernetes") {
+        include("**/*.secret.*")
+    }.files.toTypedArray()
 }
 
 task("lint") {
@@ -68,6 +77,53 @@ task("buildDocker") {
 task("push") {
     group = "publishing"
     dependsOn("jib")
+}
+
+val writeDeploymentParameters by tasks.creating {
+    group = "deployment"
+    val templateFiles = fileTree("src/deployment/kubernetes") {
+        include("**/*.tpl.*")
+    }.files
+
+    fun File.outputFile() = file(path.replace(".tpl", ".filled"))
+
+    inputs.files(templateFiles)
+    outputs.files(templateFiles.map { it.outputFile() })
+
+    doLast {
+        templateFiles.forEach { templateFile ->
+            templateFile.outputFile().writeText(
+                templateFile.readText().replace("\$DOCKER_IMAGE", dockerImage)
+            )
+        }
+    }
+}
+
+fun kubectlDeployTask(kustomizationPath: String, commonTag: Pair<String, String>, block: Exec.() -> Unit = {}) = tasks.creating(Exec::class) {
+    group = "deployment"
+    dependsOn(writeDeploymentParameters)
+
+    inputs.files(fileTree(kustomizationPath) {
+        exclude("*.encrypted")
+        exclude("*.tpl.*")
+    })
+
+    commandLine(
+        "kubectl", "apply",
+        "--kustomize", ".",
+        "--wait",
+        "--prune",
+        "--selector", "${commonTag.first}=${commonTag.second}"
+    )
+    workingDir(kustomizationPath)
+    block()
+}
+
+val deployServer by kubectlDeployTask("src/deployment/kubernetes/server", "component" to "server")
+
+task("deploy") {
+    group = "deployment"
+    dependsOn(deployServer)
 }
 
 val Project.versionDetails
