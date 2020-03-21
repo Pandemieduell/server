24c4864025beea354d86122fc881c66df775fa81
Joshua Gleitze <dev@joshuagleitze.de>
2020-03-21 13:29:06 +0200

    Setup docker build & docker run

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,14 @@ plugins {
     id("org.springframework.boot") version "2.2.5.RELEASE"
     id("io.spring.dependency-management") version "1.0.9.RELEASE"
     id("com.github.sherter.google-java-format") version "0.8"
+    id("com.google.cloud.tools.jib") version "2.1.0"
+    id("com.palantir.docker-run") version "0.25.0"
+    id("com.palantir.git-version") version "0.12.2"
 }
 
 group = "de.pandemieduell"
-version = "0.0.1-SNAPSHOT"
+version = versionDetails.gitHash
+val dockerImage = "docker.pkg.github.com/pandemieduell/server/${project.name}:${project.version}"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_11
@@ -27,6 +31,18 @@ tasks.withType<Test> {
     useJUnitPlatform()
 }
 
+
+jib {
+    to {
+        image = dockerImage
+    }
+}
+
+dockerRun {
+    name = project.name
+    image = dockerImage
+}
+
 task("lint") {
     dependsOn(tasks["verifyGoogleJavaFormat"])
 }
@@ -35,3 +51,9 @@ task("fixStyle") {
     dependsOn(tasks["googleJavaFormat"])
 }
 
+task("buildDocker") {
+    dependsOn("jibDockerBuild")
+}
+
+val Project.versionDetails
+    get() = (this.extra["versionDetails"] as groovy.lang.Closure<*>)() as com.palantir.gradle.gitversion.VersionDetails
